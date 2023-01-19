89838340a33a46ffcde14e427831fd6f64a00ed6
izzybodo <115185378+izzybodo@users.noreply.github.com>
2023-01-19 16:37:32 -0300

    delete apply-new-fonts reference

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.scss";
 
 const Footer = () => {
     return (
-        <div className={classNames("mt-4 apply-new-fonts", styles.wrapper)}>
+        <div className={classNames("mt-4", styles.wrapper)}>
             <div className={styles.contentA}>
                 Need help? <span className={styles.contentALink}>Contact support</span>
             </div>
